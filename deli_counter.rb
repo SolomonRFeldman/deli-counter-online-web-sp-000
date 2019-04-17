b0a5b2696618b2d10998f6fd51c09295e7e29c99
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-17 17:45:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,3 @@
 def line(katz_deli)
-  katz_deli.each_with_index { |person, index| "Welcome #{person}. You are number #{index + 1} in line."
+  katz_deli.each_with_index { |person, index| "Welcome #{person}. You are number #{index + 1} in line."}
 end
