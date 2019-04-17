33702959ca9d68a6768bfed48473ae0959757190
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-17 18:21:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ end
 
 def take_a_number(katz_deli, line_joiner)
   katz_deli << line_joiner
-  puts "Welcome, #{line_joiner}. You are number #{katz_deli.size + 1} in line."
+  puts "Welcome, #{line_joiner}. You are number #{katz_deli.size} in line."
 end
 
 katz_deli = ["paul", "anthony", "ahhh"]
