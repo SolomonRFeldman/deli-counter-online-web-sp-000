6d8a8134476a3e84455a7759b8714ba3e16f6767
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-17 18:14:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,12 @@
 def line(katz_deli)
-  puts "The line is currently: #{katz_deli.each_with_index {
-    |person, index|
-    katz_deli[index] = "#{index + 1}. #{person}" }.join(" ")
-  }"
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{katz_deli.each_with_index {
+      |person, index|
+      katz_deli[index] = "#{index + 1}. #{person}" }.join(" ")
+    }"
+  end
 end
 
 katz_deli = ["paul", "anthony", "ahhh"]
