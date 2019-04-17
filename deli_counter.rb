4d3d0b51b5e4012020a3069b6de0f614905895b0
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-17 18:12:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(katz_deli)
-  puts "The line is currently: 
-  #{katz_deli.each_with_index { |person, index|
+  puts "The line is currently: #{katz_deli.each_with_index {
+    |person, index|
     katz_deli[index] = "#{index + 1}. #{person}" }.join(" ")
   }"
 end
